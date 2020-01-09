a5895aa02e682d28ae7a11243355529ce87e1a25
wntduupl <houlijuntom@gmail.com>
2020-01-09 11:38:19 +0800

    test

--- server/main.go
+++ server/main.go
@@ -129,134 +129,134 @@ func main() {
 	myApp.Usage = "server(with SMUX)"
 	myApp.Version = VERSION
 	myApp.Flags = []cli.Flag{
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "listen,l",
 			Value: ":29900",
-			Usage: &"kcp server listen address",
+			Usage: "kcp server listen address",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "target, t",
 			Value: "127.0.0.1:12948",
-			Usage: &"target server address, or path/to/unix_socket",
+			Usage: "target server address, or path/to/unix_socket",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "key",
 			Value:  "it's a secrect",
 			Usage:  "pre-shared secret between client and server",
-			EnvVar: &"KCPTUN_KEY",
+			EnvVar: "KCPTUN_KEY",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "crypt",
 			Value: "aes",
 			Usage: "aes, aes-128, aes-192, salsa20, blowfish, twofish, cast5, 3des, tea, xtea, xor, sm4, none",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "mode",
 			Value: "fast",
 			Usage: "profiles: fast3, fast2, fast, normal, manual",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "mtu",
 			Value: 1350,
 			Usage: "set maximum transmission unit for UDP packets",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "sndwnd",
 			Value: 1024,
 			Usage: "set send window size(num of packets)",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "rcvwnd",
 			Value: 1024,
 			Usage: "set receive window size(num of packets)",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "datashard,ds",
 			Value: 10,
 			Usage: "set reed-solomon erasure coding - datashard",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "parityshard,ps",
 			Value: 3,
 			Usage: "set reed-solomon erasure coding - parityshard",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "dscp",
 			Value: 0,
 			Usage: "set DSCP(6bit)",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "nocomp",
 			Usage: "disable compression",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:   "acknodelay",
 			Usage:  "flush ack immediately when a packet is received",
 			Hidden: true,
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:   "nodelay",
 			Value:  0,
 			Hidden: true,
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:   "interval",
 			Value:  50,
 			Hidden: true,
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:   "resend",
 			Value:  0,
 			Hidden: true,
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:   "nc",
 			Value:  0,
 			Hidden: true,
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "sockbuf",
 			Value: 4194304, // socket buffer size in bytes
 			Usage: "per-socket buffer in bytes",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "smuxbuf",
 			Value: 4194304,
 			Usage: "the overall de-mux buffer in bytes",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "keepalive",
 			Value: 10, // nat keepalive interval in seconds
 			Usage: "seconds between heartbeats",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "snmplog",
 			Value: "",
 			Usage: "collect snmp to file, aware of timeformat in golang, like: ./snmp-20060102.log",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "snmpperiod",
 			Value: 60,
 			Usage: "snmp collect period, in seconds",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "pprof",
 			Usage: "start profiling server on :6060",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "log",
 			Value: "",
 			Usage: "specify a log file to output, default goes to stderr",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "quiet",
 			Usage: "to suppress the 'stream open/close' messages",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "tcp",
 			Usage: "to emulate a TCP connection(linux)",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "c",
 			Value: "", // when the value is not empty, the config path must exists
 			Usage: "config from json file, which will override the command from shell",
